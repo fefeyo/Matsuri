028aedd2a404839a4b425481105c8cdb280dcc67
sakatayu <imanity.fefe@gmail.com>
2015-11-22 16:20:27 +0900

    'search'

--- app/src/main/java/com/fefeyo/matsuri/MainActivity.java
+++ app/src/main/java/com/fefeyo/matsuri/MainActivity.java
@@ -1,15 +1,38 @@
 package com.fefeyo.matsuri;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.graphics.Movie;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import com.beardedhen.androidbootstrap.BootstrapButton;
+import com.fefeyo.matsuri.item.MovieItem;
+import com.fefeyo.matsuri.item.ResultAdapter;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.OnClick;
+import cz.msebera.android.httpclient.Header;
 
 /**
  * Dwangoはてなハッカソン
@@ -21,44 +44,110 @@ public class MainActivity extends AppCompatActivity {
      * from=の後に秒数
      */
     final static String postUrl = "http://www.nicovideo.jp/watch/sm27491227?from=30";
+    final static String END = "http://api.search.nicovideo.jp/api/v2/video/contents/search?";
+    /**
+     * 後にcontentIdをつけることで動画のページヘ
+     */
+    final static String VIDEO_URL = "http://nico.ms/";
+
+    private AsyncHttpClient mClient;
+    private ArrayList<MovieItem> mItems;
+
+    private ProgressDialog dialog;
+
+    @InjectView(R.id.search_text)
+    EditText search_text;
+    @InjectView(R.id.search_button)
+    BootstrapButton search_button;
+    @InjectView(R.id.result_list)
+    ListView result_list;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        ButterKnife.inject(this);
+        mClient = new AsyncHttpClient();
+    }
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Uri uri = Uri.parse(postUrl);
-                Intent i = new Intent(Intent.ACTION_VIEW,uri);
-                startActivity(i);
+    /**
+     * 動画を検索する
+     */
+    private void searchVideo(final AsyncHttpClient client, final String search_word) {
+        dialog = new ProgressDialog(this);
+        dialog.setMessage("検索中・・・");
+        dialog.show();
+        mItems = new ArrayList<>();
+        client.get(
+                getApplicationContext(),
+                getNicoNicoQuery(search_word),
+                new AsyncHttpResponseHandler() {
+                    @Override
+                    public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                        try {
+                            final String result = new String(responseBody, "UTF-8");
+                            final JSONObject parent = new JSONObject(result);
+                            final JSONArray datas = parent.getJSONArray("data");
+                            for(int i = 0; i < datas.length(); i++){
+                                final JSONObject data = datas.getJSONObject(i);
+                                final MovieItem item = new MovieItem();
+                                item.setMovieUrl(VIDEO_URL + data.getString("contentId"));
+                                item.setNumber(data.getString("viewCounter"));
+                                item.setPostdate(data.getString("startTime"));
+                                item.setThmbnailUrl(data.getString("thumbnailUrl"));
+                                item.setTitle(data.getString("title"));
+                                item.setComments(null);
+                                mItems.add(item);
+                            }
+                            result_list.setAdapter(new ResultAdapter(
+                                    getApplicationContext(),
+                                    0,
+                                    mItems
+                            ));
+                            dialog.dismiss();
+                        } catch (UnsupportedEncodingException | JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+                        Log.i("接続エラー", "接続エラー");
+                        dialog.dismiss();
+                    }
+                }
+        );
+    }
+
+    /**
+     * ニコニコ横断検索API用URLを作成
+     */
+    private String getNicoNicoQuery(final String q) {
+        String[] queryWords = new String[]{
+                "q=" + q,
+                "targets=title",
+                "fields=contentId,title,description,viewCounter,startTime,thumbnailUrl",
+                "_sort=-viewCounter",
+                "_limit=10",
+                "_context=Matsuri"
+        };
+        final String searator = "&";
+
+        final StringBuilder sb = new StringBuilder();
+        for (String word : queryWords) {
+            if (sb.length() > 0) {
+                sb.append(searator);
             }
-        });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+            sb.append(word);
         }
+        String queryUrl = END + sb.toString();
 
-        return super.onOptionsItemSelected(item);
+        return queryUrl;
+    }
+
+    @OnClick(R.id.search_button)
+    void onClick(final View v) {
+        searchVideo(mClient, search_text.getText().toString());
     }
 }
---
+++ app/src/main/java/com/fefeyo/matsuri/item/MovieItem.java
@@ -0,0 +1,64 @@
+package com.fefeyo.matsuri.item;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * Created by USER on 2015/11/22.
+ */
+public class MovieItem {
+    private String title;
+    private String postdate;
+    private String thmbnailUrl;
+    private String number;
+    private String movieUrl;
+    private HashMap<Integer, ArrayList<String>> comments;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getPostdate() {
+        return postdate;
+    }
+
+    public void setPostdate(String postdate) {
+        this.postdate = postdate;
+    }
+
+    public String getThmbnailUrl() {
+        return thmbnailUrl;
+    }
+
+    public void setThmbnailUrl(String thmbnailUrl) {
+        this.thmbnailUrl = thmbnailUrl;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getMovieUrl() {
+        return movieUrl;
+    }
+
+    public void setMovieUrl(String movieUrl) {
+        this.movieUrl = movieUrl;
+    }
+
+    public HashMap<Integer, ArrayList<String>> getComments() {
+        return comments;
+    }
+
+    public void setComments(HashMap<Integer, ArrayList<String>> comments) {
+        this.comments = comments;
+    }
+}
